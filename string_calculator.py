720615ce69a277650ca5763427cd474ddbd4c595
Ibrahim Mohammed <46675479+Ibby19@users.noreply.github.com>
2019-05-15 09:56:42 +0200

    Add string calculator

--- string_calculator.py
+++ string_calculator.py
@@ -10,10 +10,31 @@ def add(*num):
 
     for numbers in num:
         if numbers != "":
+            numbers = any_delimiter(numbers)
             numbers = second_delimter(numbers)
             total += sum(map(int, numbers.split(delimiter)))
     return total
-
+        
 def second_delimter(numbers):
     """ This function splits a string by the new line delimiter"""
     return numbers.replace("\n", delimiter)
+
+def any_delimiter(numbers):
+    if re.match('//.{1}\n', numbers):
+        d_separator = numbers[2]
+        numbers = numbers[4:]
+        numbers = numbers.replace(d_separator,delimiter)
+    return numbers
+
+def sum_string_numbers(string):
+    """ This function splits the string by a delimiter
+    then it checks if the numbers in the string are negative"""
+    numbers = map(int, string.split(','))
+    invalid_number(numbers)
+    return sum(numbers)
+
+def invalid_number(numbers):
+    """ This function checks if a number in the string is negative
+    then returns a value error"""
+    if any(number < 0 for number in numbers):
+        raise ValueError ("You can't use a negative number")
