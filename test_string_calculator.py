1ac2ca23155222c0d2b8b7c25779db790a0d7ec1
Ibrahim Mohammed <46675479+Ibby19@users.noreply.github.com>
2019-05-15 09:57:40 +0200

    Add tests for string calculator

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,4 +1,4 @@
-from string_1 import *
+from string_calculator import add,sum_string_numbers
 import pytest
 
 def test_add_and_empty_string():
@@ -18,3 +18,19 @@ def test_sum_multiple_numbers():
     assert add('10,20,30') == 60
     assert add('5,5,5,5') == 20
     assert add('2,4,6') == 12
+
+def test_random_amount_of_nums():
+    assert add('1,2','4,5') == 12
+    assert add('1,2','4,5','') == 12
+
+def test_random_delimiter():
+    assert add("//;\n3;2") == 5
+
+def test_newline():
+    assert add("2\n4,6") == 12
+
+def test_negative_numbers():
+    with pytest.raises(ValueError):
+        sum_string_numbers('-1,-2')
+        sum_string_numbers('-1')
+        sum_string_numbers('-1,-2,3')
